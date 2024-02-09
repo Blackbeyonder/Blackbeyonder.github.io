f4ca7e6a34b55eee8e71d1571c0122e71694a03f
Blackbeyonder <54973451+Blackbeyonder@users.noreply.github.com>
2024-02-09 11:52:53 -0600

    test circle

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+
+// Llamar a la función con el porcentaje deseado (por ejemplo, 50 para el 50%)
+document.addEventListener('DOMContentLoaded', function() {
+    // Llamar a la función setPercentage después de que se cargue el DOM
+    console.log("dom1");
+    // setPercentage(50);
+  });
+  
