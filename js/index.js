aec1e62b394eae037305b9a91f2b67ba132dbfda
Blackbeyonder <54973451+Blackbeyonder@users.noreply.github.com>
2024-02-09 18:37:24 -0600

    union progrees bar with circle

--- js/index.js
+++ js/index.js
@@ -5,5 +5,14 @@ document.addEventListener('DOMContentLoaded', function() {
     // Llamar a la función setPercentage después de que se cargue el DOM
     console.log("dom1");
     // setPercentage(50);
+
+       const circle = document.getElementById('circle');
+      const redPath = document.querySelector('.red');
+      
+      redPath.addEventListener('animationend', () => {
+        console.log("animado");
+        circle.style.border = '5px solid #2EB51A'; // Cambiar el color del círculo al rojo
+      });
+   
   });
   
